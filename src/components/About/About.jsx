5b49278f1c4a97e62cf476796376cba6730f2ea5
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-06-09 23:05:21 +0800

    feat(about): update contents

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,89 @@
+import { ChevronsUpLeft } from "tabler-icons-react"
+
 const About = () => {
-    return (<section className="bg-gray-800 pattern py-20" id="about">
+    return (
+        <section className="bg-gray-800 pattern py-20" id="about">
+            <div className="max-w-5xl px-6 mx-auto text-center flex justify-center md:justify-start">
+                <h2 className="text-2xl font-semibold text-white border-b-2 border-warning w-fit flex">
+                    <ChevronsUpLeft />
+                    About Me
+                </h2>
+            </div>
+
+            <div className="flex container mx-auto my-4 flex-col lg:flex-row">
+                <SkillSet />
+                <Divider up={<Par />} down={<Stat />} />
+            </div>
+
+        </section>
+    )
+
+}
+
+const SkillSet = () => {
+    return (
+        <div className="flex justify-center">
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">My Skills</h2>
+                    <ul>
+                        <li>Frontend Developer</li>
+                        <li>Backend Developer</li>
+                        <li>Database Administrator</li>
+                        <li>QA Tester</li>
+                        <li>Automation Specialist</li>
+                    </ul>
+                </div></div>
+        </div>
+    )
+}
+
+const Divider = ({ up, down }) => {
+    return (
+        <div className="flex flex-col w-full border-opacity-50">
+            <div className="grid h-fit card rounded-box place-items-center">{up}</div>
+            <div className="divider"></div>
+            <div className="grid h-fit card rounded-box place-items-center">{down}</div>
+        </div>
+    )
+}
+
+const Par = () => {
+    return (
         <div className="max-w-5xl px-6 mx-auto text-center">
-            <h2 className="text-2xl font-semibold text-white">About Me</h2>
-            <p className="text-gray-500 mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ullamcorper
-                nulla nunc quis molestie volutpat elementum at. Ultrices ipsum, enim
-                cursus lorem ac. Orci maecenas praesent arcu eget orci est orci
-                nullam. Leo purus est pellentesque massa at tortor, est. Aliquet
-                pulvinar a mattis sagittis. Suspendisse porta id elementum, massa.
+            <p className="mt-4 text-slate-100">
+                I'm a <span className="text-warning">Fullstack Web Developer</span> from Sariaya Quezon (Philippines).
+                Turning <span className="text-error">complex</span> problem into a <span className="text-success">simple</span> and <span className="text-info">manageable</span> Web Information System is my forte.
+                I earned my Bachelor's degree in <span className="text-success">Information Technology</span> in the year of <span className="text-warning">2021</span>.
             </p>
         </div>
-    </section>)
+    )
+}
 
+const Stat = () => {
+    return (
+        <div className="stats shadow stats-vertical md:stats-horizontal">
+
+            <div className="stat place-items-center">
+                <div className="stat-title">Freelance Experience</div>
+                <div className="stat-value">3 years</div>
+                <div className="stat-desc">Fullstack Development</div>
+            </div>
+
+            <div className="stat place-items-center">
+                <div className="stat-title">Professional Experience</div>
+                <div className="stat-value text-secondary">2 years</div>
+                <div className="stat-desc text-secondary">Fullstack Web Development</div>
+            </div>
+
+            <div className="stat place-items-center">
+                <div className="stat-title">Availability</div>
+                <div className="stat-value">24/7</div>
+                <div className="stat-desc">Message me anytime</div>
+            </div>
+
+        </div>
+    )
 }
 
 export default About
