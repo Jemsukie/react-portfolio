9a8eb7d8deb70b029b2a434040c5481eff4e9a8f
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-06-09 22:00:38 +0800

    feat(): update navbar ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useRef } from "react";
+import { Home, Menu } from 'tabler-icons-react'
 
 const Navbar = () => {
     const navLinks = [
-        { title: 'Home', href: '#home' },
+        { title: 'Home', href: '#home', icon: <Home /> },
         { title: 'About Me', href: '#about' },
         { title: 'Projects', href: '#project' },
-        { title: 'Testimony', href: '#testimony' },
+        { title: 'Works', href: '#works' },
     ]
 
     const navbarRef = useRef(null);
@@ -15,10 +16,10 @@ const Navbar = () => {
         document.body.style.paddingTop = `${navbarRef.current.offsetHeight}px`;
     }, []);
 
-    return (<nav ref={navbarRef} className="fixed top-0 z-10 w-full bg-white shadow-lg">
+    return (<nav ref={navbarRef} className="fixed top-0 z-10 w-full shadow-2xl bg-secondary border-b-2 border-info">
         <div className="container mx-auto justify-between max-w-8xl flex p-5 flex-row items-center">
-            <a href="#" className="flex title-font font-medium items-center text-gray-50 mb-4 md:mb-0">
-                <span className="ml-3 text-xl font-bold text-success">Devfolio</span>
+            <a href="#home" className="flex title-font font-medium items-center text-gray-50 mb-4 md:mb-0">
+                <span className="ml-3 text-xl font-bold text-accent">Devfolio</span>
             </a>
 
             <WebMenu navLinks={navLinks} />
@@ -50,22 +51,22 @@ const MenuIcon = () => {
 }
 
 const WebMenu = ({ navLinks }) => {
-    return (<div className="sm:inline-flex hidden">
+    return (<div className="sm:inline-flex hidden p-4">
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
             {navLinks.map(n => (
-                <a key={n.title} href={n.href} className="mx-2 btn btn-outline btn-info font-bold p-4 border-b-2 border-primary hover:border-accent">{n.title}</a>
+                <a key={n.title} href={n.href} className="mx-2 font-bold hover:border-accent"><span className="text-slate-200 hover:text-info flex btn">{n.icon}{n.title}</span></a>
             ))}
         </nav>
     </div>)
 }
 
 const MobileMenu = ({ navLinks }) => {
-    return (<div className="md:hidden sm:hidden inline-flex">
+    return (<div className="md:hidden sm:hidden inline-flex p-4">
         <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="mx-2 btn btn-outline btn-info font-bold p-4 border-b-2 border-primary hover:border-accent"><MenuIcon /></label>
-            <ul id="navitem" tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 border-2 border-primary">
+            <label tabIndex={0} className="mx-2 btn btn-outline btn-info font-bold border-b-2 border-primary hover:border-accent"><Menu /></label>
+            <ul id="navitem" tabIndex={0} className="dropdown-content menu shadow bg-base-100 rounded-box w-52 border-2 border-primary">
                 {navLinks.map(n => (
-                    <a key={n.title} href={n.href}>{n.title}</a>
+                    <li key={n.title}><a className="btn btn-ghost" href={n.href}>{n.title}</a></li>
                 ))}
             </ul>
         </div>
