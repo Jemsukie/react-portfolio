0dc5940db8eec3832d24b4242af66cb117033df7
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-06-10 13:39:43 +0800

    feat(about): add tech stacks

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react"
 import { ChevronsUpLeft } from "tabler-icons-react"
+import assets from "../../lib/Assets"
 
 const About = () => {
     return (
         <section className="bg-gray-800 pattern py-20" id="about">
             <div className="max-w-5xl px-6 mx-auto text-center flex justify-center md:justify-start">
-                <h2 className="text-2xl font-semibold text-white border-b-2 border-warning w-fit flex">
+                <h2 className="text-2xl font-semibold text-slate-200 border-b-2 border-warning w-fit flex">
                     <ChevronsUpLeft />
                     About Me
                 </h2>
@@ -25,15 +27,12 @@ const SkillSet = () => {
         <div className="flex justify-center">
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title">My Skills</h2>
-                    <ul>
-                        <li>Frontend Developer</li>
-                        <li>Backend Developer</li>
-                        <li>Database Administrator</li>
-                        <li>QA Tester</li>
-                        <li>Automation Specialist</li>
-                    </ul>
-                </div></div>
+                    <h2 className="card-title">
+                        <div className="tooltip tooltip-open tooltip-right tooltip-info" data-tip="Check these out!"><span>My Skills and Techs</span></div>
+                    </h2>
+                    <Techs />
+                </div>
+            </div>
         </div>
     )
 }
@@ -70,10 +69,10 @@ const Stat = () => {
                 <div className="stat-desc">Fullstack Development</div>
             </div>
 
-            <div className="stat place-items-center">
-                <div className="stat-title">Professional Experience</div>
-                <div className="stat-value text-secondary">2 years</div>
-                <div className="stat-desc text-secondary">Fullstack Web Development</div>
+            <div className="stat place-items-center bg-slate-800">
+                <div className="stat-title text-slate-400">Professional Experience</div>
+                <div className="stat-value  text-slate-100">2 years</div>
+                <div className="stat-desc  text-slate-100">Fullstack Web Development</div>
             </div>
 
             <div className="stat place-items-center">
@@ -84,6 +83,121 @@ const Stat = () => {
 
         </div>
     )
+}
+
+const Techs = () => {
+    const [swapOn, setSwapOn] = useState(<></>)
+    const {
+        devtools,
+        hubspot,
+        jest,
+        playwright,
+        storybook,
+        temporal,
+        testproject,
+        zapier,
+        mongodb,
+        firebase,
+        postgresql,
+        cubejs,
+        html,
+        css,
+        js,
+        bootstrap,
+        tailwind,
+        reactjs,
+        webflow,
+        php,
+        graphql,
+        expressjs,
+        nodejs
+    } = assets
+
+    const menuLinks = {
+        'frontend': {
+            title: 'Frontend Development',
+            swap: [
+                { brand: 'HTML', image: html },
+                { brand: 'CSS', image: css },
+                { brand: 'JavaScript', image: js },
+                { brand: 'Bootstrap', image: bootstrap },
+                { brand: 'Tailwind', image: tailwind },
+                { brand: 'React JS', image: reactjs },
+                { brand: 'Webflow', image: webflow },
+            ]
+        },
+        'backend': {
+            title: 'Backend Development',
+            swap: [
+                { brand: 'JavaScript', image: js },
+                { brand: 'PHP', image: php },
+                { brand: 'GraphQL', image: graphql },
+                { brand: 'Express JS', image: expressjs },
+                { brand: 'Node JS', image: nodejs },
+            ]
+        },
+        'database': {
+            title: 'Database Administration',
+            swap: [
+                { brand: 'MongoDB', image: mongodb },
+                { brand: 'Firebase', image: firebase },
+                { brand: 'PostgreSQL', image: postgresql },
+                { brand: 'CubeJS', image: cubejs },
+            ]
+        },
+        'testing': {
+            title: 'QA Testing',
+            swap: [
+                { brand: 'Playwright', image: playwright },
+                { brand: 'Jest', image: jest },
+                { brand: 'Storybook', image: storybook },
+                { brand: 'Testproject', image: testproject },
+                { brand: 'Chrome Devtools and Extensions', image: devtools },
+            ]
+        },
+        'automation': {
+            title: 'Workflow Automation',
+            swap: [
+                { brand: 'HubSpot', image: hubspot },
+                { brand: 'Zapier', image: zapier },
+                { brand: 'Temporal IO', image: temporal },
+            ]
+        },
+    }
+
+    const changeSwapOn = (item) => setSwapOn(
+        <ul className="text-slate-800">
+            {menuLinks[item].swap.map((s, idx) => <li key={idx}><ItemAvatars image={s.image} />{s.brand}</li>)}
+        </ul>
+    )
+
+    const menu = (
+        <ul className="text-slate-800">
+            {Object.keys(menuLinks).map(m => <li key={m}
+                onClick={() => { changeSwapOn(m) }}
+                className="hover:border-success border-b-2 border-transparent"
+            >{menuLinks[m].title}</li>)}
+        </ul>
+    )
+
+    return (
+        <label className="swap swap-flip justify-start">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" />
+
+            <div className="swap-on">{swapOn}</div>
+            <div className="swap-off">{menu}</div>
+        </label>
+    )
+}
+
+const ItemAvatars = ({ image }) => {
+    return (
+        <div className="avatar mr-2">
+            <div className="w-4 rounded">
+                <img src={image} alt="No img" />
+            </div>
+        </div>)
 }
 
 export default About
