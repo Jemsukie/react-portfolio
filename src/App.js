b50c98e84abca80dd7cf6558232c27d8891fdd8c
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-06-10 16:51:16 +0800

    fix(project): update parent dependent on deleted link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
-import Projects from './Projects'
+import Projects from './components/Projects/Projects'
 import './index.css'
 
 function App() {
