9b66c4fa557c4fe5ad486582adfb52d5acf08618
Jemuel Lupo <jemuel.lupo@gmail.com>
2023-06-10 13:39:03 +0800

    feat(): initialize and import assets

---
+++ src/lib/Assets.js
@@ -0,0 +1,67 @@
+import img1 from "../assets/projects/1.png"
+import img2 from "../assets/projects/2.png"
+import img3 from "../assets/projects/3.png"
+import img4 from "../assets/projects/4.png"
+import img5 from "../assets/projects/5.png"
+import img6 from "../assets/projects/6.png"
+import bg from '../assets/hero/bg.jpg'
+import hero from '../assets/hero/jem.png'
+import storybook from '../assets/logo/storybook.png'
+import playwright from '../assets/logo/playwright.png'
+import jest from '../assets/logo/jest.png'
+import testproject from '../assets/logo/testproject.png'
+import devtools from '../assets/logo/devtools.png'
+import hubspot from '../assets/logo/hubspot.png'
+import zapier from '../assets/logo/zapier.png'
+import temporal from '../assets/logo/temporal.png'
+import mongodb from '../assets/logo/mongodb.png'
+import firebase from '../assets/logo/firebase.png'
+import postgresql from '../assets/logo/postgresql.png'
+import cubejs from '../assets/logo/cubejs.png'
+import html from '../assets/logo/html.png'
+import css from '../assets/logo/css.png'
+import js from '../assets/logo/js.png'
+import bootstrap from '../assets/logo/bootstrap.png'
+import tailwind from '../assets/logo/tailwind.png'
+import reactjs from '../assets/logo/reactjs.png'
+import webflow from '../assets/logo/webflow.png'
+import php from '../assets/logo/php.png'
+import graphql from '../assets/logo/graphql.png'
+import expressjs from '../assets/logo/expressjs.png'
+import nodejs from '../assets/logo/nodejs.png'
+
+const assets = {
+    img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    bg,
+    hero,
+    storybook,
+    playwright,
+    jest,
+    testproject,
+    devtools,
+    hubspot,
+    zapier,
+    temporal,
+    mongodb,
+    firebase,
+    postgresql,
+    cubejs,
+    html,
+    css,
+    js,
+    bootstrap,
+    tailwind,
+    reactjs,
+    webflow,
+    php,
+    graphql,
+    expressjs,
+    nodejs
+}
+
+export default assets
