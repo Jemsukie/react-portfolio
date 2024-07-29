78f8fe1e6c6070b3c72dedb8f79895c94f44e7e9
Jemuel Lupo <jemuel.lupo@gmail.com>
2024-07-29 17:18:38 +0800

    chore(): update cv information

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -20,7 +20,10 @@ const Hero = ({ reference }: TReferenceProps) => {
                     <img src={hero} className="w-full max-w-xs rounded-full bg-gradient-to-r from-[#FF7F28] to-[#36D399] p-1" alt='No img' />
                 </div>
                 <div className='p-4 text-center md:text-start'>
-                    <h1 className="text-5xl font-bold text-slate-200 flex flex-col sm:flex-row justify-center md:justify-start">Hi I&apos;m&nbsp;<span className="text-warning border-b-2 border-info xs:w-full cursor-pointer">Jemuel Lupo</span></h1>
+                    <h1 className="text-5xl font-bold text-slate-200 flex flex-col sm:flex-row justify-center md:justify-start">
+                        Hi I&apos;m&nbsp;
+                        <span className="text-warning border-b-2 border-info xs:w-full cursor-pointer">Jemuel Lupo</span>
+                    </h1>
                     <p className="mt-6 text-success text-2xl">Software Engineer</p>
                     <p className="mt-2 md:mx-0 mx-10 text-white text-md">Empowering businesses with streamlined digital systems and organizational efficiency</p>
                     <div className="mt-2 flex justify-center md:justify-start">
