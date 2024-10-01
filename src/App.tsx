842dec42d6afe524ae1e6241df807c20d888ba8f
Jemuel Lupo <jemuel.lupo@sb2024.live>
2024-10-01 09:31:02 +0800

    feat(): connect to ws

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,17 @@ function App() {
     contact: useRef(null),
   }
 
+  const ws = new WebSocket('ws://192.168.5.52:8080')
+
+  ws.onopen = () => {
+    console.log('Connected to WebSocket server')
+  }
+
+  ws.onmessage = event => {
+    const li = document.createElement('li')
+    li.textContent = event.data
+  }
+
   return (
     <div className="App">
       <main className="bg-body font-Montserrat">
