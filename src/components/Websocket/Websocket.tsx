28e126a25a219092a3d8dbfd2f7a7d43977d67ef
Jemuel Lupo <jemuel.lupo@sb2024.live>
2024-10-01 15:01:19 +0800

    feat(): use hook for connecting to Websocket

--- src/components/Websocket/Websocket.tsx
+++ src/components/Websocket/Websocket.tsx
@@ -1,56 +1,76 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import useWebsocketConnection from '../../hooks/useWebsocketConnection'
 
 const Websocket: React.FC = () => {
-    const [socket, setSocket] = useState<WebSocket | null>(null)
-    const [connectionStatus, setConnectionStatus] = useState<string>('Not Connected')
-    const websocketUrl = '192.168.5.45:8080' // Your server IP and port
-    const wsPrefix = 'ws://'
 
-    // Function to create WebSocket connection
-    const createWebSocket = (url: string): void => {
-        const newSocket = new WebSocket(url)
-
-        newSocket.onopen = () => {
-            setConnectionStatus('Connected')
-            console.log('WebSocket connection established:', url)
-        }
-
-        newSocket.onmessage = (event: MessageEvent) => {
-            console.log('Message from server:', event.data)
-        }
-
-        newSocket.onerror = (error: Event) => {
-            console.error('WebSocket error:', error)
-        }
-
-        newSocket.onclose = () => {
-            setConnectionStatus('Disconnected')
-            console.log('WebSocket connection closed')
-        }
-
-        setSocket(newSocket) // Set the new WebSocket instance to state
-    }
-
+    const { connection } = useWebsocketConnection('ws://192.168.5.52:8080')
     useEffect(() => {
-        // Only connect via WebSocket if the page is served over HTTP
-        if(!socket){
-            if (window.location.protocol === 'http:') {
-                console.log('Page loaded over HTTP, attempting WebSocket connection...')
-                createWebSocket(`${wsPrefix}${websocketUrl}`)
-            } else {
-                console.log('Page loaded over HTTPS, WebSocket connection not allowed.')
+        if (connection) {
+
+            console.log('--this is connection', connection)
+
+            connection.onmessage = () => {
+                console.log('Hello open!')
             }
         }
+    }, [connection])
 
-        // Cleanup on component unmount
-        return () => {
-            socket?.close() // Close the WebSocket connection if it exists
-        }
-    }, [socket]) // Add socket as a dependency to ensure cleanup works correctly
+
+
+    // const [socket, setSocket] = useState<WebSocket | null>(null)
+    // const [connectionStatus, setConnectionStatus] = useState<string>('Not Connected')
+    // const websocketUrl = '192.168.5.45:8080' // Your server IP and port
+    // const wsPrefix = 'ws://'
+
+    // // Function to create WebSocket connection
+    // const createWebSocket = (url: string): void => {
+    //     const newSocket = new WebSocket(url)
+
+    //     newSocket.onopen = () => {
+    //         setConnectionStatus('Connected')
+    //         console.log('WebSocket connection established:', url)
+    //     }
+
+    //     newSocket.onmessage = (event: MessageEvent) => {
+    //         console.log('Message from server:', event.data)
+    //     }
+
+    //     newSocket.onerror = (error: Event) => {
+    //         console.error('WebSocket error:', error)
+    //     }
+
+    //     newSocket.onclose = () => {
+    //         setConnectionStatus('Disconnected')
+    //         console.log('WebSocket connection closed')
+    //     }
+
+    //     setSocket(newSocket) // Set the new WebSocket instance to state
+    // }
+
+    // useEffect(() => {
+    //     // Only connect via WebSocket if the page is served over HTTP
+    //     if(!socket){
+    //         if (window.location.protocol === 'http:') {
+    //             console.log('Page loaded over HTTP, attempting WebSocket connection...')
+    //             createWebSocket(`${wsPrefix}${websocketUrl}`)
+    //         } else {
+    //             console.log('Page loaded over HTTPS, WebSocket connection not allowed.')
+    //         }
+    //     }
+
+    //     // Cleanup on component unmount
+    //     return () => {
+    //         socket?.close() // Close the WebSocket connection if it exists
+    //     }
+    // }, [socket]) // Add socket as a dependency to ensure cleanup works correctly
 
     return (
         <div>
-            <h1>WebSocket Connection Status: {connectionStatus}</h1>
+            <button onClick={() => {
+                if(connection){
+                    connection.send('Helllllloooooo')
+                }
+            }}>WS</button>
         </div>
     )
 }
---
+++ src/hooks/useWebsocketConnection.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+const useWebsocketConnection = (link: string): {connection: WebSocket | null, error: Event | null } => {
+    const [connection, setConnection] = useState<WebSocket | null>(null)
+    const [error, setError] = useState<Event | null>(null)
+
+    useEffect(() => {
+        const ws = new WebSocket(link)
+
+        ws.onopen = () => {
+            setConnection(ws)
+        }
+        ws.onclose = () => {
+            setConnection(null)
+        }
+        ws.onerror = (event) => {
+            setConnection(null)
+            setError(event)
+        }
+    }, [])
+
+
+
+    return { connection, error }
+}
+
+export default useWebsocketConnection
