304af461f41722368437318370770ff39e590f5e
Jemsukie <jemuel.lupo@gmail.com>
2025-04-26 03:16:04 +0800

    chore(): update manifest json

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -7,9 +8,13 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-root.render(<React.StrictMode>
-    <App />
-  </React.StrictMode>)
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -17,10 +22,7 @@ root.render(<React.StrictMode>
 serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
-
 // to log results (for example: reportWebVitals(console.log))
-
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
 reportWebVitals()
 
