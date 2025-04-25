b5f1d0b5e7a59de63edd1292d5be7f66109b90ff
Jemsukie <jemuel.lupo@gmail.com>
2025-04-26 02:50:12 +0800

    fix(): escape unused vars

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -18,10 +18,12 @@ const isLocalhost = Boolean(
       window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
   )
 
+  /* eslint-disable no-unused-vars */
   type Config = {
     onSuccess?: (registration: ServiceWorkerRegistration) => void;
     onUpdate?: (registration: ServiceWorkerRegistration) => void;
   };
+  /* eslint-enable no-unused-vars */
 
   export function register(config?: Config) {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
