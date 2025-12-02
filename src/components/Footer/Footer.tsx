114d6b0a534268c839b156c362743468fcbbbfd1
Jemsukie <jemuel.lupo@gmail.com>
2025-12-02 19:17:29 +0800

    docs(): update resume

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ const
         return (
             <footer className={`footer footer-center p-4 text-slate-200 justify-center ${className}`}>
                 <div className="flex md:flex-row flex-col-reverse">
-                    <p>Copyright ©{dateYear} - Jemuel Lupo</p>
+                    <p>Copyright ©{dateYear} - Uel Versed Labs</p>
                     <div className="avatar mr-2">
                         <div className="w-16 rounded-full border-slate-200 hover:border-info border-2 hover-enlarge">
                             <img onClick={() => goToSection(reference)} src={hero} className="cursor-pointer" alt="No img" />
