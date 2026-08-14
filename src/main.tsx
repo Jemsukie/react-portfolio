a20c2aeff03b5994c4debd9d310fd551eeb84dba
JemDevvy <jemuel@nimbusplus.io>
2026-08-14 21:41:54 +0800

    faet(): update PWA service worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if ('serviceWorker' in navigator) {
+  void (async () => {
+    const registrations = await navigator.serviceWorker.getRegistrations()
+    await Promise.all(
+      registrations.map((registration) => registration.unregister()),
+    )
+    const cacheKeys = await caches.keys()
+    await Promise.all(cacheKeys.map((key) => caches.delete(key)))
+  })()
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
